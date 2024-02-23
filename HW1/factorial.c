5a834bce2d92a5280e72779ca129e9470f711524
Andrew Shustrov <141773395+999ashu@users.noreply.github.com>
2024-02-23 19:02:53 +0300

    fix of incorrect width

--- HW1/factorial.c
+++ HW1/factorial.c
@@ -2,20 +2,14 @@
 #include <stdio.h>
 
 #define BASE 10
-#define M_31 INT32_MAX;
+#define M_31 INT32_MAX
 #define MODULE UINT16_MAX
+#define DIV2(x) (x >> 1)
+#define MOD2(x) (x & 1)
 
 uint8_t num_width, fact_width;
 
-uint32_t get_factorial(const uint16_t num)
-{
-	uint64_t factorial = 1;
-	for (uint64_t i = 2; i <= num; i++)
-		factorial = factorial * i % M_31;
-	return factorial;
-}
-
-uint8_t count_digits(uint32_t num)
+uint8_t number_len(uint32_t num)
 {
 	uint8_t num_size = 0;
 	do
@@ -26,14 +20,35 @@ uint8_t count_digits(uint32_t num)
 	return num_size;
 }
 
+uint8_t biggest_factorial_len(const uint16_t num)
+{
+	uint8_t counter = 1;
+	uint64_t factorial = 1;
+	for (uint64_t i = 2; i <= num; i++)
+	{
+		factorial = factorial * i % M_31;
+		const uint8_t len = number_len(factorial);
+		counter = len > counter ? len : counter;
+		if (counter == 10)
+		{
+			break;
+		}
+	}
+	return counter;
+}
+
 void print_cover()
 {
 	printf("+");
 	for (uint8_t i = 0; i < num_width + 2; i++)
+	{
 		printf("-");
+	}
 	printf("+");
 	for (uint8_t i = 0; i < fact_width + 2; i++)
+	{
 		printf("-");
+	}
 	printf("+\n");
 }
 
@@ -50,15 +65,15 @@ void print_header(const int8_t align)
 		const uint8_t fact_spaces = fact_width - 2;
 		printf(
 			"| %*s%c%*s | %*s%s%*s |\n",
-			(num_spaces >> 1) + (num_spaces & 1),
+			DIV2(num_spaces) + MOD2(num_spaces),
 			"",
 			'n',
-			num_spaces >> 1,
+			DIV2(num_spaces),
 			"",
-			(fact_spaces >> 1) + (fact_spaces & 1),
+			DIV2(fact_spaces) + MOD2(fact_spaces),
 			"",
 			"n!",
-			fact_spaces >> 1,
+			DIV2(fact_spaces),
 			"");
 	}
 	print_cover();
@@ -66,29 +81,36 @@ void print_header(const int8_t align)
 
 void print_data(const uint16_t start, const uint16_t end, const int8_t align)
 {
-	uint64_t fact = start > 0 ? get_factorial(start - 1) : 1;
+	uint64_t fact = 1;
+	if (start > 0)
+	{
+		for (uint16_t i = 2; i <= start - 1; i++)
+		{
+			fact = fact * i % M_31;
+		}
+	}
 	for (uint32_t num = start; num <= end; num++)
 	{
 		fact = fact * (num == 0 ? 1 : num) % M_31;
 		if (align)
 		{
-			printf("| %*d | %*ld |\n", align * num_width, num, align * fact_width, fact);
+			printf("| %*d | %*lld |\n", align * num_width, num, align * fact_width, fact);
 		}
 		else
 		{
-			const uint8_t num_spaces = num_width - count_digits(num);
-			const uint8_t fact_spaces = fact_width - count_digits(fact);
+			const uint8_t num_spaces = num_width - number_len(num);
+			const uint8_t fact_spaces = fact_width - number_len(fact);
 			printf(
-				"| %*s%d%*s | %*s%ld%*s |\n",
-				(num_spaces >> 1) + (num_spaces & 1),
+				"| %*s%d%*s | %*s%lld%*s |\n",
+				DIV2(num_spaces) + MOD2(num_spaces),
 				"",
 				num,
-				num_spaces >> 1,
+				DIV2(num_spaces),
 				"",
-				(fact_spaces >> 1) + (fact_spaces & 1),
+				DIV2(fact_spaces) + MOD2(fact_spaces),
 				"",
 				fact,
-				fact_spaces >> 1,
+				DIV2(fact_spaces),
 				"");
 		}
 	}
@@ -103,22 +125,21 @@ int main(void)
 	{
 		if (n_start <= n_end)
 		{
-			num_width = count_digits(n_end);
-			const uint8_t temp = count_digits(get_factorial(n_end));
+			num_width = number_len(n_end);
+			const uint8_t temp = biggest_factorial_len(n_end);
 			fact_width = temp != 1 ? temp : 2;
 			print_header(align);
 			print_data(n_start, n_end, align);
-			print_cover();
 		}
 		else
 		{
-			num_width = count_digits(MODULE);
-			fact_width = count_digits(get_factorial(MODULE));
+			num_width = number_len(MODULE);
+			fact_width = biggest_factorial_len(MODULE);
 			print_header(align);
 			print_data(n_start, MODULE, align);
 			print_data(0, n_end, align);
-			print_cover();
 		}
+		print_cover();
 		return 0;
 	}
 	if (n_start < 0 || n_end < 0)
